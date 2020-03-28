269d1a9cd882bfb5a97b95228b025eb44627551e
Scott Huffman <scott@shuffman.com>
2020-03-28 14:19:40 -0500

    updated init script

--- setup-env.bash
+++ setup-env.bash
@@ -1,8 +1,25 @@
+# -------------------
+# Setup Firewall
+# -------------------
+
 apt-get install ufw
+
+# Plex
+ufw allow 32400
+
+# OVPN
+ufw allow 943
 ufw allow 9443
 ufw allow 1194
+
+# Minecraft
 ufw allow 4326
 ufw allow 4327
+ufw allow 25565
+
+# -------------------
+# Setup Directories
+# -------------------
 
 mkdir /apps
 
@@ -16,3 +33,9 @@ mkdir /apps/plex/config
 mkdir /apps/plex/tv
 mkdir /apps/plex/movies
 mkdir /apps/plex/workout
+
+# -------------------
+# Execute
+# -------------------
+
+docker-compose up -d
