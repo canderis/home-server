7cc96561d30466c67dfd006ea57ed95127781070
Scott Huffman <scott@shuffman.com>
2020-03-28 13:49:28 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # home-server
+
+## Configure Env
+
+### Install docker
+
+### Setup Folders
---
+++ setup-env.bash
@@ -0,0 +1,18 @@
+apt-get install ufw
+ufw allow 9443
+ufw allow 1194
+ufw allow 4326
+ufw allow 4327
+
+mkdir /apps
+
+mkdir /apps/minecraft
+mkdir /apps/minecraft/rcon
+
+mkdir /apps/ovpn
+
+mkdir /apps/plex
+mkdir /apps/plex/config
+mkdir /apps/plex/tv
+mkdir /apps/plex/movies
+mkdir /apps/plex/workout
